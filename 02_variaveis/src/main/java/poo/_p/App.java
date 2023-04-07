5d07d46646fd70e2d0f5fdaede76d348354c005b
Marcos <estudogit@outlook.com>
2023-04-07 18:57:20 -0300

    variaveis

---
+++ 02_variaveis/src/main/java/poo/_p/App.java
@@ -0,0 +1,11 @@
+package poo._p;
+
+public class App {
+
+    public static void main(String[] args) {
+        String nome = "ASD";
+        int num = 53;
+        System.out.printf("nome: %s, num: %d", nome, num);
+    }
+    
+}
