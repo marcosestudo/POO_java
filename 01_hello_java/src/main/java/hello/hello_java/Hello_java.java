3e822648b8282bf407b63cd1f1de9a243ddbe2d6
Marcos <estudogit@outlook.com>
2023-04-07 18:32:22 -0300

    hello numerado

--- 01_hello_java/src/main/java/hello/hello_java/Hello_java.java
+++ 01_hello_java/src/main/java/hello/hello_java/Hello_java.java
@@ -3,6 +3,6 @@ package hello.hello_java;
 public class Hello_java {
 
     public static void main(String[] args) {
-        System.out.println("Hello JAVA!");
+        System.out.println("Hello, JAVA!");
     }
 }
