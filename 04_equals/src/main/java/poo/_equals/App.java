92c7276146f0fd5ccab240ace5621f28599a7e30
Marcos <estudogit@outlook.com>
2023-04-08 16:36:40 -0300

    equals

---
+++ 04_equals/src/main/java/poo/_equals/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo._equals;
+
+/**
+ *
+ * @author marcos
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
