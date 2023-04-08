73cfbbad9a22593e18a9067f6d3c87e46d32cf5e
Marcos <estudogit@outlook.com>
2023-04-08 16:33:03 -0300

    if else refatorado

--- 03_if_else/src/main/java/poo/_if_else/App.java
+++ 03_if_else/src/main/java/poo/_if_else/App.java
@@ -3,11 +3,11 @@ package poo._if_else;
 public class App {
 
     public static void main(String[] args) {
-        String letra = "c";
+        String letra = "b";
 
-        if (letra.equals("a")) {
+        if (letra == "a") {
             System.out.println(1);
-        } else if (letra.equals("b")) {
+        } else if (letra == "b") {
             System.out.println(2);
         } else {
             System.out.println(3);
