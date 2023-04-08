637e17296d32c4f1978a33e5bb89a4e738e5d0a1
Marcos <estudogit@outlook.com>
2023-04-08 16:31:26 -0300

    if else

---
+++ 03_if_else/src/main/java/poo/_if_else/App.java
@@ -0,0 +1,17 @@
+package poo._if_else;
+
+public class App {
+
+    public static void main(String[] args) {
+        String letra = "c";
+
+        if (letra.equals("a")) {
+            System.out.println(1);
+        } else if (letra.equals("b")) {
+            System.out.println(2);
+        } else {
+            System.out.println(3);
+        }
+
+    }
+}
